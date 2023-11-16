e4baa4ec225d8415983607b06617f80f3a5244b5
ThanapatKwanmuang <thanapat.kwanmuang@elev.ga.ntig.se>
2023-11-16 14:01:01 +0100

    classes pt2

--- vinterprojekt.py
+++ vinterprojekt.py
@@ -1,25 +1,97 @@
 import random 
 
 class Fighter:
+    Strength =
+    Dexterity = 
+    Constitution =
+    Intelligence = 
+    Wisdom = 
+    Charisma = 
 
 class Rogue:
+    Strength =
+    Dexterity = 
+    Constitution =
+    Intelligence = 
+    Wisdom = 
+    Charisma =
 
 class Barbarian:
+    Strength =
+    Dexterity = 
+    Constitution =
+    Intelligence = 
+    Wisdom = 
+    Charisma =
 
-class Bard: 
+class Bard:
+    Strength =
+    Dexterity = 
+    Constitution =
+    Intelligence = 
+    Wisdom = 
+    Charisma =
 
 class Cleric:
+    Strength =
+    Dexterity = 
+    Constitution =
+    Intelligence = 
+    Wisdom = 
+    Charisma =
 
-class Monk: 
+class Monk:
+    Strength =
+    Dexterity = 
+    Constitution =
+    Intelligence = 
+    Wisdom = 
+    Charisma =
 
 class Paladin:
+    Strength =
+    Dexterity = 
+    Constitution =
+    Intelligence = 
+    Wisdom = 
+    Charisma =
 
 class Ranger:
+    Strength =
+    Dexterity = 
+    Constitution =
+    Intelligence = 
+    Wisdom = 
+    Charisma =
 
 class Druid: 
+    Strength =
+    Dexterity = 
+    Constitution =
+    Intelligence = 
+    Wisdom = 
+    Charisma =
 
 class Sorcerer:
+    Strength =
+    Dexterity = 
+    Constitution =
+    Intelligence = 
+    Wisdom = 
+    Charisma =
 
-class Warlock: 
+class Warlock:
+    Strength =
+    Dexterity = 
+    Constitution =
+    Intelligence = 
+    Wisdom = 
+    Charisma =
 
 class Wizard:
+    Strength =
+    Dexterity = 
+    Constitution =
+    Intelligence = 
+    Wisdom = 
+    Charisma =
