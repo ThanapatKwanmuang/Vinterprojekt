bebda543dbb9495d5bc1e3ecaf139f092e0fc09e
ThanapatKwanmuang <thanapat.kwanmuang@elev.ga.ntig.se>
2023-11-16 13:54:07 +0100

    classes

---
+++ vinterprojekt.py
@@ -0,0 +1,25 @@
+import random 
+
+class Fighter:
+
+class Rogue:
+
+class Barbarian:
+
+class Bard: 
+
+class Cleric:
+
+class Monk: 
+
+class Paladin:
+
+class Ranger:
+
+class Druid: 
+
+class Sorcerer:
+
+class Warlock: 
+
+class Wizard:
